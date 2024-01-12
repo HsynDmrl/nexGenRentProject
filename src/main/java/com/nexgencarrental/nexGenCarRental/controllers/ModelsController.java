49e37096fd8f5214233568f2a7658132216820e5
Hüseyin Demirel <138596916+HsynDmrl@users.noreply.github.com>
2024-01-12 21:19:33 +0300

    Update ModelsController.java

--- src/main/java/com/nexgencarrental/nexGenCarRental/controllers/ModelsController.java
+++ src/main/java/com/nexgencarrental/nexGenCarRental/controllers/ModelsController.java
@@ -16,6 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping("api/models")
 @AllArgsConstructor
+@CrossOrigin
 public class ModelsController {
     private final ModelService modelService;
     @GetMapping("/getAll")
