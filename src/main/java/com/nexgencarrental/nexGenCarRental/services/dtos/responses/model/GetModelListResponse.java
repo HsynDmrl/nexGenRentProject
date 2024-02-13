2f9302e9bbdceadbdcb4b7b9f2a237cf4c768833
Hüseyin Demirel <138596916+HsynDmrl@users.noreply.github.com>
2024-02-13 17:24:21 +0300

    Update GetModelListResponse.java
    response updated.

--- src/main/java/com/nexgencarrental/nexGenCarRental/services/dtos/responses/model/GetModelListResponse.java
+++ src/main/java/com/nexgencarrental/nexGenCarRental/services/dtos/responses/model/GetModelListResponse.java
@@ -1,5 +1,6 @@
 package com.nexgencarrental.nexGenCarRental.services.dtos.responses.model;
 
+import com.nexgencarrental.nexGenCarRental.services.dtos.responses.brand.GetBrandListResponse;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
